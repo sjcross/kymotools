4bb08acf4bde034dacdce692d959ffd46f2f3a47
Stephen Cross <stephencross86@gmail.com>
2023-02-15 18:12:55 +0000

    Minor updates

--- ctraptools/kymos/interactive.py
+++ ctraptools/kymos/interactive.py
@@ -1,7 +1,9 @@
 import matplotlib as mpl
 import matplotlib.pyplot as plt
+import numpy as np
 
 from matplotlib.widgets import Slider, SpanSelector
+from scipy.signal import butter, sosfiltfilt
 
 class DisplayRangeSelector:
     def __init__(self, kymo, px_min=0, px_max=255):
@@ -68,14 +70,17 @@ class TraceAnalyser:
         self._px_min = px_min
         self._px_max = px_max
 
+        self._fig = None
         self._crop_plot_ax = None
         self._filt_crop_plot = None
 
-        self._span_selector_kymo = None
         self._span_selector_trace = None
         self._range_min = 0
         self._range_max = 1
-        
+
+        self._freq_slider = None
+        self._order_slider = None
+                
         self._create_plot(kymo)
 
     def _create_plot(self, kymo):
@@ -84,14 +89,9 @@ class TraceAnalyser:
             self._range_max = range_max
             self._update_plot()
             
-            # sos = butter(butter_order, butter_freq, 'lp', output='sos')
-            # raw_data['Filtered '+channel] =  signal.sosfiltfilt(sos, raw_data[channel])
-            # global data
-            # data = raw_data.loc[(raw_data['Time (s)'] > xmin) & (raw_data['Time (s)'] < xmax)]
-            
         # Initialising the plot
-        fig, ((ax1, ax2), (ax3, ax4), (self._crop_plot_ax, ax6)) = plt.subplots(3, 2, figsize=(11, 11), gridspec_kw={'width_ratios': [4, 1], 'height_ratios': [1, 1, 1.8]})
-        fig.subplots_adjust(hspace=0.1, wspace=0.06)
+        self._fig, ((ax1, ax2), (ax3, ax4), (self._crop_plot_ax, self._hist_ax)) = plt.subplots(3, 2, figsize=(11, 11), gridspec_kw={'width_ratios': [4, 1], 'height_ratios': [1, 1, 1.8]})
+        self._fig.subplots_adjust(hspace=0.1, wspace=0.06)
 
         # Kymograph image
         kymo_line_length = kymo.size_um[0]
@@ -102,15 +102,6 @@ class TraceAnalyser:
         ax1.tick_params(top=True, labeltop=True, bottom=False, labelbottom=False)
         ax1.set_xlabel("Time (s)")  
         ax1.xaxis.set_label_position('top') 
-        self._span_selector_kymo = SpanSelector(
-            ax1,
-            on_span_select,
-            "horizontal",
-            useblit=True,
-            props=dict(alpha=0.5, facecolor="tab:blue"),
-            interactive=True,
-            drag_from_anywhere=True
-        )
 
         ax2.axis('off')
 
@@ -135,32 +126,66 @@ class TraceAnalyser:
         self._crop_plot_ax.set_xlabel("Time (s)")
         self._crop_plot_ax.set_ylabel(self._y_label)
         self._crop_plot_ax.plot(self._time_s, self._data, label='Raw data', color='black', alpha=.3)
-        self._filt_crop_plot = self._crop_plot_ax.plot([], [], label='Filtered data')
+        self._filt_crop_plot, = self._crop_plot_ax.plot([], [], label='Filtered data')
 
         # Histogram of values in cropped trace
-        ax6.set_xlabel("Counts")
-        ax6.set_title('Frequencies (raw force)', fontsize=8)
-        ax6.get_shared_y_axes().join(self._crop_plot_ax, ax6)
-        ax6.set_yticklabels([])
-        ax6.minorticks_on()
-        ax6.yaxis.set_tick_params(which='minor', bottom=False)
+        self._hist_ax.set_xlabel("Counts")
+        self._hist_ax.set_title('Frequencies (raw force)', fontsize=8)
+        self._hist_ax.get_shared_y_axes().join(self._crop_plot_ax, self._hist_ax)
+        self._hist_ax.set_yticklabels([])
+        self._hist_ax.minorticks_on()
+        self._hist_ax.yaxis.set_tick_params(which='minor', bottom=False)
+        self._hist, = self._hist_ax.plot([], [], color='black')
+
+        # adjust the main plot to make room for the sliders
+        self._fig.subplots_adjust(left=0.25, bottom=0.32)
+
+        # Make a horizontal slider to set the filter frequency:
+        self._freq_slider = Slider(
+            ax=self._fig.add_axes([0.25, 0.2, 0.65, 0.03]),
+            label='Butterworth filter frequency',
+            valmin=0.1,
+            valmax=1,
+            valinit=0.4,
+            valstep=0.05
+        )
+        
+        # Make a horizontal slider to set the filter order:
+        self._order_slider = Slider(
+            ax=self._fig.add_axes([0.25, 0.17, 0.65, 0.03]),
+            label='Butterworth filter order',
+            valmin=1,
+            valmax=20,
+            valinit=1,
+            valstep=1
+        )
+
+        # We can just call the standard _update_plot function
+        def update_slider(val):
+            self._update_plot()
+
+        self._freq_slider.on_changed(update_slider)
+        self._order_slider.on_changed(update_slider)
 
     def _update_plot(self):
-        # region_y2 = data['Filtered '+channel]
+        curr_time_s = self._time_s[(self._time_s > self._range_min) & (self._time_s < self._range_max)]
+        curr_data = self._data[(self._time_s > self._range_min) & (self._time_s < self._range_max)]
+        sos = butter(self._order_slider.val, self._freq_slider.val, 'lp', output='sos')
+        filtered_data =  sosfiltfilt(sos, curr_data)
         
-        # counts, bins = np.histogram(data[channel], bins=150)
-        # bin_mids = (bins[1:] + bins[:-1])/2
+        counts, bins = np.histogram(curr_data, bins=150)
+        bin_mids = (bins[1:] + bins[:-1])/2
         
-        # region_min = data[channel].min()
-        # region_max = data[channel].max()
-        # region_range = data[channel].max() - data[channel].min()
+        region_min = self._data.min()
+        region_max = self._data.max()
+        region_range = region_max - region_min
 
-        if len(self._time_s) >= 2:    
+        if len(self._time_s) >= 2:
             self._crop_plot_ax.set_xlim(self._range_min, self._range_max)
-            # ax5.set_ylim(region_min- (region_range*0.05), region_max + (region_range*0.05))
-            # ax5.legend(fontsize=9)
-            # plot3.set_data(region_x, region_y2)
-            
-            # ax6.set_xlim(0, counts.max())
-            # plot4.set_data(counts, bin_mids)
-            # fig.canvas.draw_idle()
+            self._crop_plot_ax.set_ylim(region_min - (region_range*0.05), region_max + (region_range*0.05))
+            self._crop_plot_ax.legend(fontsize=9)
+            self._filt_crop_plot.set_data(curr_time_s, filtered_data)
+                        
+            self._hist_ax.set_xlim(0, counts.max())
+            self._hist.set_data(counts, bin_mids)
+            self._fig.canvas.draw_idle()
